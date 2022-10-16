6697fa85b444583a9506356bfcd6b2e5c96a3637
GABOBB <gabbol2002@gmail.com>
2022-10-16 11:14:40 -0600

    paquete estructuras de datos

---
+++ S_controlador/src/main/java/estructuras_de_datos/Nodo_D_E.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package estructuras_de_datos;
+
+/**
+ *
+ * @author Gabriel
+ */
+public class Nodo_D_E {
+    
+}
