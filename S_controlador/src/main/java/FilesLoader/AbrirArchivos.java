be2312f35e06a8ee89dc2a26be8d06c7fc60c520
Michael <suarezmichael@estudiantec.cr>
2022-10-30 14:32:42 -0600

    abrir archivos

---
+++ S_controlador/src/main/java/FilesLoader/AbrirArchivos.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package FilesLoader;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Admin
+ */
+public class AbrirArchivos {
+    
+    public static void abrir(String id) throws IOException{
+        
+        ProcessBuilder p = new ProcessBuilder();
+
+        p.command("cmd.exe", "/c", id);
+        
+        p.start();
+
+    }
+    
+}
